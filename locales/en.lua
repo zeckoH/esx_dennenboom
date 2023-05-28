145e4d6cf21c656da393196b865d582863176aff
zeckoH <37878668+zeckoh@users.noreply.github.com>
2023-05-28 16:53:36 +0200

    Create en.lua

---
+++ locales/en.lua
@@ -0,0 +1,16 @@
+Locales['en'] = {
+  ['shop_robbery'] = 'Winkel overval',
+  ['press_to_rob'] = 'Druk ~INPUT_CONTEXT~ om de bank te ~o~overvallen~s~ ~b~%s~s~',
+  ['robbery_timer'] = 'Je moet nog: ~r~%s~s~ seconde wachten',
+  ['recently_robbed'] = 'this bank was recently been robbed. Please wait ~y~%s~s~ seconds until you can rob again',
+  ['rob_in_prog'] = '~r~Overval bezig bij: ~b~%s~s~',
+  ['started_to_rob'] = 'Je hebt de overval gestart ~y~%s~s~',
+  ['alarm_triggered'] = 'Het alarm is afgegaan',
+  ['robbery_complete'] = '~r~De overval is gelukt ~s~, je hebt ~g~$%s~s~ ~o~gestolen~s~',
+  ['robbery_complete_at'] = '~r~Overval is gelukt bij: ~y~%s~s~',
+  ['robbery_cancelled'] = 'De overval is gestopt!',
+  ['robbery_cancelled_at'] = '~r~De overval is gestopt bij: ~b~%s~s~!',
+  ['min_police'] = 'Er moet tenminste ~b~%s politie~s~ in de stad zijn om de bank te kunnen overvallen.',
+  ['robbery_already'] = '~r~ Er is al een overval bezig.',
+  ['no_threat'] = 'Je vormt geen bedreiging voor de bank!',
+}
